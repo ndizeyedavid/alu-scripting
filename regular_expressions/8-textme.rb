44310f0b82a5b3bed9a3ddc35486de541100d8e2
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-09-03 11:25:53 +0200

    ffinal final fix: added a flatten and compact method to trim out rejected paterns and merge the desired sender and receiver and flag fields

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(\+*[a-zA-Z0-9]+)|to:(\+*[0-9]+)|flags:([-0-9:]+)/).join(", ")
+puts ARGV[0].scan(/from:(\+*[a-zA-Z0-9]+)|to:(\+*[0-9]+)|flags:([-0-9:]+)/).flatten.compact.join(",")
